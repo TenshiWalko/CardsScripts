d31d54f07328db42ae3d60405404df3817ddd53e
TenshiWalko <efwalko2013@gmail.com>
2024-08-01 17:46:37 -0400

    Add files via upload

--- c3105200027.lua
+++ c3105200027.lua
@@ -83,7 +83,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-		e1:SetValue(c:GetAttack()*2)
+		e1:SetValue(c:GetAttack()+1000)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE|RESET_PHASE|PHASE_END)
 		c:RegisterEffect(e1)
 		-- Destroy it during the End Phase of this turn
